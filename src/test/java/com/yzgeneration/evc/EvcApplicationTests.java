d7401de371e8b27880da44fa80485d6fd6902dd2
jiyunio <jiyun3269@naver.com>
2024-12-26 18:18:32 +0900

    Initial commit

---
+++ src/test/java/com/yzgeneration/evc/EvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzgeneration.evc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
