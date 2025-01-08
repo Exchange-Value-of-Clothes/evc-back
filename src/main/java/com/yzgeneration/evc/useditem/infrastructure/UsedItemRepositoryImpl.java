27b9edd9c256784fcc15956260818f82f82b6b74
jiyunio <jiyun3269@naver.com>
2025-01-08 21:08:34 +0900

    [Feat] 중고상품 등록 관련 서비스 기능 추가 #10

--- src/main/java/com/yzgeneration/evc/useditem/infrastructure/UsedItemRepositoryImpl.java
+++ src/main/java/com/yzgeneration/evc/useditem/infrastructure/UsedItemRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.yzgeneration.evc.useditem.infrastructure;
 
+import com.yzgeneration.evc.useditem.model.UsedItem;
 import com.yzgeneration.evc.useditem.service.port.UsedItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,8 @@ import org.springframework.stereotype.Repository;
 public class UsedItemRepositoryImpl implements UsedItemRepository {
     private final UsedItemJpaRepository usedItemJpaRepository;
 
+    @Override
+    public UsedItem save(UsedItem usedItem) {
+        return usedItemJpaRepository.save(UsedItemEntity.from(usedItem)).toModel();
+    }
 }
