a07b42624050bb82ab95ea5917bbfafd37863f3e
jiyunio <jiyun3269@naver.com>
2025-01-16 17:23:59 +0900

    [Refactor] 중고상품 이미지 repository 어노테이션 위치변경 #12

--- src/main/java/com/yzgeneration/evc/image/service/port/UsedItemImageRepository.java
+++ src/main/java/com/yzgeneration/evc/image/service/port/UsedItemImageRepository.java
@@ -1,9 +1,7 @@
 package com.yzgeneration.evc.image.service.port;
 
 import com.yzgeneration.evc.image.model.UsedItemImage;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UsedItemImageRepository {
     UsedItemImage save(UsedItemImage usedItemImage);
 }
