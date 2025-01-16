ee25b46bc88a6005afa704849bb37a340c32f166
jiyunio <jiyun3269@naver.com>
2025-01-16 15:17:18 +0900

    [Refactor] 중고상품 등록시 db에 이미지 저장 서비스 뼈대 #12

--- src/main/java/com/yzgeneration/evc/useditem/service/UsedItemService.java
+++ src/main/java/com/yzgeneration/evc/useditem/service/UsedItemService.java
@@ -1,18 +1,26 @@
 package com.yzgeneration.evc.useditem.service;
 
+import com.yzgeneration.evc.image.service.port.UsedItemImageRepository;
 import com.yzgeneration.evc.useditem.dto.UsedItemRequest.CreateUsedItem;
 import com.yzgeneration.evc.useditem.dto.UsedItemResponse;
 import com.yzgeneration.evc.useditem.model.UsedItem;
 import com.yzgeneration.evc.useditem.service.port.UsedItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UsedItemService {
     private final UsedItemRepository usedItemRepository;
+    private final UsedItemImageRepository usedItemImageRepository;
 
-    public UsedItemResponse createUsedITem(CreateUsedItem createUsedItem) {
-        return UsedItemResponse.from(usedItemRepository.save(UsedItem.create(createUsedItem)));
+    public UsedItemResponse createUsedITem(CreateUsedItem createUsedItem, List<MultipartFile> imageFiles) {
+        UsedItem usedItem = usedItemRepository.save(UsedItem.create(createUsedItem));
+        // [notice] 이미지 생성 로직 추가 & return 값 변경
+        return UsedItemResponse.from(usedItem, new ArrayList<>());
     }
 }
