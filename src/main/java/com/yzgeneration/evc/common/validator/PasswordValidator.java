ef28a33c157a6f78cc43356679a632a1101147de
gujunhyeong <rnwnsgud90@naver.com>
2025-01-27 16:15:18 +0900

    [Refactor] if문 부정조건문 수정

--- src/main/java/com/yzgeneration/evc/common/validator/PasswordValidator.java
+++ src/main/java/com/yzgeneration/evc/common/validator/PasswordValidator.java
@@ -7,14 +7,13 @@ import java.util.regex.Pattern;
 
 import static com.yzgeneration.evc.common.exception.ErrorCode.*;
 
-public class PasswordValidator {
+public abstract class PasswordValidator {
     private static final String PASSWORD_REGEX = "^(?!.*\\s).{8,}$";
     private static final Pattern pattern = Pattern.compile(PASSWORD_REGEX);
 
     public static void validate(String password) {
         Matcher matcher = pattern.matcher(password);
-        if (!matcher.matches()) {
-            throw new CustomException(PASSWORD_INCORRECT_FORMAT);
-        }
+        if (matcher.matches()) return;
+        throw new CustomException(INVALID_PASSWORD);
     }
 }
