74e3897ba5289125ea4cfb88f2405b9ffcdb8923
gujunhyeong <rnwnsgud90@naver.com>
2025-01-27 16:10:32 +0900

    [Feat] 시큐리티

---
+++ src/main/java/com/yzgeneration/evc/security/JwtAuthenticationFilter.java
@@ -0,0 +1,65 @@
+package com.yzgeneration.evc.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yzgeneration.evc.authentication.implement.TokenProvider;
+import com.yzgeneration.evc.common.exception.ErrorCode;
+import com.yzgeneration.evc.common.exception.ErrorResponse;
+import com.yzgeneration.evc.domain.member.service.port.MemberRepository;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final TokenProvider tokenProvider;
+    private final ObjectMapper objectMapper;
+    private final MemberRepository memberRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String accessToken = parseToken(request.getHeader("Authorization"));
+            Long memberId = tokenProvider.getMemberId(accessToken);
+            MemberPrincipal memberPrincipal = new MemberPrincipal(memberRepository.getById(memberId));
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    memberPrincipal,
+                    memberPrincipal.getMember().getId(),
+                    memberPrincipal.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            filterChain.doFilter(request, response);
+        } catch (ExpiredJwtException e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json;charset=utf-8");
+            response.getWriter().write(objectMapper.writeValueAsString(ErrorResponse.from(ErrorCode.TOKEN_UNAUTHORIZED)));
+        }
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request)  {
+        String[] excludePath = {"/api/auth", "/api/members/register", "/api/members/register/verify"};
+        String path = request.getRequestURI();
+        return Arrays.stream(excludePath).anyMatch(path::matches);
+    }
+
+    private String parseToken(String authorization) {
+        if (StringUtils.hasText(authorization) && authorization.startsWith("Bearer ")) {
+            return authorization.substring(7);
+        }
+        return null;
+    }
+
+}
--- src/main/java/com/yzgeneration/evc/security/SecurityConfig.java
+++ src/main/java/com/yzgeneration/evc/security/SecurityConfig.java
@@ -1,14 +1,15 @@
-package com.yzgeneration.evc.common.config;
+package com.yzgeneration.evc.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -17,16 +18,21 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .cors(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
-//                .exceptionHandling(config -> config.authenticationEntryPoint())
+                .exceptionHandling(config -> config.authenticationEntryPoint(customAuthenticationEntryPoint))
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/**").permitAll())
                 .headers(hhc -> hhc.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -41,7 +47,6 @@ public class SecurityConfig {
         configuration.addAllowedMethod("*");
         configuration.addAllowedOriginPattern("*");
         configuration.setAllowCredentials(true);
-        configuration.addExposedHeader("ACCESS_TOKEN");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
