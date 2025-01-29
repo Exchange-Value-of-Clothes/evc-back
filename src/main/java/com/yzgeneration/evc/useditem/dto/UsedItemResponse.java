94fe271dff0209e4972050d81db3f3fb262c185f
jiyunio <jiyun3269@naver.com>
2025-01-30 02:00:05 +0900

    [Refactor] record를 이용해 response 수정 #17

--- src/main/java/com/yzgeneration/evc/useditem/dto/UsedItemResponse.java
+++ src/main/java/com/yzgeneration/evc/useditem/dto/UsedItemResponse.java
@@ -1,6 +1,8 @@
 package com.yzgeneration.evc.useditem.dto;
 
 import com.yzgeneration.evc.useditem.enums.UsedItemStatus;
+import com.yzgeneration.evc.useditem.model.ItemDetails;
+import com.yzgeneration.evc.useditem.model.ItemStats;
 import com.yzgeneration.evc.useditem.model.UsedItem;
 import com.yzgeneration.evc.useditem.model.UsedItemTransaction;
 import lombok.Builder;
@@ -9,32 +11,16 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Builder
-public class UsedItemResponse {
-    private final Long memberId;
-    private final String title;
-    private final String category;
-    private final String content;
-    private final int price;
-    private final UsedItemTransaction usedItemTransaction;
-    private final UsedItemStatus usedItemStatus;
-    private final int viewCount;
-    private final int likeCount;
-    private final int chattingCount;
-    private final List<String> imageURLs;
-    private final LocalDateTime createAt;
-
-    public static UsedItemResponse of(UsedItem usedItem, List<String> imageURLs) {
+public record UsedItemResponse(Long memberId, ItemDetails itemDetails, UsedItemTransaction usedItemTransaction,
+                               UsedItemStatus usedItemStatus, ItemStats itemStats, List<String> imageURLs,
+                               LocalDateTime createAt) {
+    public static UsedItemResponse of(UsedItem usedItem, List<String> imageURLs){
         return UsedItemResponse.builder()
                 .memberId(usedItem.getId())
-                .title(usedItem.getTitle())
-                .category(usedItem.getCategory())
-                .content(usedItem.getContent())
-                .price(usedItem.getPrice())
+                .itemDetails(usedItem.getItemDetails())
                 .usedItemTransaction(usedItem.getUsedItemTransaction())
                 .usedItemStatus(usedItem.getUsedItemStatus())
-                .viewCount(usedItem.getViewCount())
-                .likeCount(usedItem.getLikeCount())
-                .chattingCount(usedItem.getChattingCount())
+                .itemStats(usedItem.getItemStats())
                 .imageURLs(imageURLs)
                 .createAt(usedItem.getCreatedAt())
                 .build();
