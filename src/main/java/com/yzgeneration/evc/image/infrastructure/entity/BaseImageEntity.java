ad97441f11990d1b19a315914864946bfafd91e2
jiyunio <jiyun3269@naver.com>
2025-01-30 02:03:39 +0900

    [Refactor] ImageEntity 생성자 접근제한 수정 #17

--- src/main/java/com/yzgeneration/evc/image/infrastructure/entity/BaseImageEntity.java
+++ src/main/java/com/yzgeneration/evc/image/infrastructure/entity/BaseImageEntity.java
@@ -9,8 +9,8 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Getter
 @SuperBuilder
 @MappedSuperclass
-@NoArgsConstructor
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @EntityListeners(value = {AuditingEntityListener.class})
 public abstract class BaseImageEntity {
     //[notice] S3 버킷 연결하며 수정될 예정 (지금은 뼈대 단계)
