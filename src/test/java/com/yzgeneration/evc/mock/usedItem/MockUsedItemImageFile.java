801a940e514f59d0736c140bca328a6ce4913eb2
jiyunio <jiyun3269@naver.com>
2025-02-18 14:31:52 +0900

    [Feat] 중고상품 등록에 대한 테스트 코드 작성 #17

--- src/test/java/com/yzgeneration/evc/mock/usedItem/MockUsedItemImageFile.java
+++ src/test/java/com/yzgeneration/evc/mock/usedItem/MockUsedItemImageFile.java
@@ -4,13 +4,12 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MockUsedItemImageFile {
 
-    public List<MultipartFile> getImageFiles(){
+    public List<MultipartFile> getImageFiles() {
         List<MultipartFile> imageFiles = new ArrayList<>();
         imageFiles.add(new MockMultipartFile("usedItem1", "usedItem1.png", MediaType.MULTIPART_FORM_DATA_VALUE, "usedItem1".getBytes()));
         imageFiles.add(new MockMultipartFile("usedItem2", "usedItem2.png", MediaType.MULTIPART_FORM_DATA_VALUE, "usedItem2".getBytes()));
