7c961ca91ab1b32156c239c9fe1c9e26e85989ab
jiyunio <jiyun3269@naver.com>
2025-02-19 02:16:54 +0900

    [Fix] git 충돌 해결

--- src/test/java/com/yzgeneration/evc/fixture/usedItem/UsedItemFixture.java
+++ src/test/java/com/yzgeneration/evc/fixture/usedItem/UsedItemFixture.java
@@ -1,28 +1,15 @@
 package com.yzgeneration.evc.fixture.usedItem;
 
-import com.navercorp.fixturemonkey.FixtureMonkey;
-import com.navercorp.fixturemonkey.api.introspector.BuilderArbitraryIntrospector;
-import com.navercorp.fixturemonkey.api.introspector.FailoverIntrospector;
-import com.navercorp.fixturemonkey.api.introspector.FieldReflectionArbitraryIntrospector;
 import com.yzgeneration.evc.domain.useditem.dto.ItemRequest.CreateItemDetails;
 import com.yzgeneration.evc.domain.useditem.dto.ItemRequest.CreateTransaction;
 import com.yzgeneration.evc.domain.useditem.dto.UsedItemRequest.CreateUsedItem;
 import com.yzgeneration.evc.domain.useditem.enums.TransactionMode;
 import com.yzgeneration.evc.domain.useditem.enums.TransactionType;
-
-import java.util.List;
+import com.yzgeneration.evc.fixture.Fixture;
 
 public abstract class UsedItemFixture {
-
-    public static final FixtureMonkey fixtureMonkey = FixtureMonkey.builder()
-            .objectIntrospector(new FailoverIntrospector(
-                    List.of(BuilderArbitraryIntrospector.INSTANCE, FieldReflectionArbitraryIntrospector.INSTANCE)
-            ))
-            .defaultNotNull(true)
-            .build();
-
     public static CreateItemDetails fixCreateItemDetails() {
-        return fixtureMonkey.giveMeBuilder(CreateItemDetails.class)
+        return Fixture.fixtureMonkey.giveMeBuilder(CreateItemDetails.class)
                 .set("title", "Test UsedItem")
                 .set("category", "top shirt")
                 .set("content", "buy right now")
@@ -31,14 +18,14 @@ public abstract class UsedItemFixture {
     }
 
     public static CreateTransaction fixCreateTransaction() {
-        return fixtureMonkey.giveMeBuilder(CreateTransaction.class)
+        return Fixture.fixtureMonkey.giveMeBuilder(CreateTransaction.class)
                 .set("transactionType", TransactionType.DIRECT)
                 .set("transactionMode", TransactionMode.BUY)
                 .sample();
     }
 
     public static CreateUsedItem fixCreateUsedItem() {
-        return fixtureMonkey.giveMeBuilder(CreateUsedItem.class)
+        return Fixture.fixtureMonkey.giveMeBuilder(CreateUsedItem.class)
                 .set("memberId", 1L)
                 .set("createItemDetails", fixCreateItemDetails())
                 .set("createTransaction", fixCreateTransaction())
