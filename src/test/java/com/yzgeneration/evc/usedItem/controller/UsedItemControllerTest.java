cca5d9348c71ad6943b51f027b6d40c8f7915714
jiyunio <jiyun3269@naver.com>
2025-02-20 18:56:50 +0900

    [Refactor] filter 검사 제외 #107

--- src/test/java/com/yzgeneration/evc/usedItem/controller/UsedItemControllerTest.java
+++ src/test/java/com/yzgeneration/evc/usedItem/controller/UsedItemControllerTest.java
@@ -14,12 +14,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -32,7 +35,10 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = UsedItemController.class)
+@WebMvcTest(controllers = UsedItemController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = OncePerRequestFilter.class),
+        })
 class UsedItemControllerTest {
 
     @Autowired
