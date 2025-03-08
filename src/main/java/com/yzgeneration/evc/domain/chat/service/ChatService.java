d5828a104ccfe5ce37d257745e2d3719cc0c7296
gujunhyeong <rnwnsgud90@naver.com>
2025-03-09 01:25:37 +0900

    [Refactor] Implement 레이어 사용

--- src/main/java/com/yzgeneration/evc/domain/chat/service/ChatService.java
+++ src/main/java/com/yzgeneration/evc/domain/chat/service/ChatService.java
@@ -3,23 +3,18 @@ package com.yzgeneration.evc.domain.chat.service;
 import com.yzgeneration.evc.common.dto.SliceResponse;
 import com.yzgeneration.evc.domain.chat.dto.*;
 import com.yzgeneration.evc.domain.chat.implement.*;
-import com.yzgeneration.evc.domain.chat.infrastructure.ChatMemberRepository;
 import com.yzgeneration.evc.domain.chat.infrastructure.ChatMessageRepository;
-import com.yzgeneration.evc.domain.chat.infrastructure.ChatRoomRepository;
-import com.yzgeneration.evc.domain.chat.model.ChatMember;
+
 import com.yzgeneration.evc.domain.chat.model.ChatMessage;
 import com.yzgeneration.evc.domain.chat.model.ChatRoom;
-import com.yzgeneration.evc.exception.CustomException;
-import com.yzgeneration.evc.exception.ErrorCode;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+
 
 import static com.yzgeneration.evc.domain.chat.SessionConstant.CHAT_ROOM_KEY;
 import static com.yzgeneration.evc.domain.chat.SessionConstant.MEMBER_KEY;
@@ -29,23 +24,15 @@ import static com.yzgeneration.evc.domain.chat.SessionConstant.MEMBER_KEY;
 @RequiredArgsConstructor
 public class ChatService {
 
-    private final ChatRoomRepository chatRoomRepository;
-    private final ChatMemberRepository chatMemberRepository;
+    private final ChatRoomManager chatRoomManager;
     private final ChatMessageRepository chatMessageRepository;
     private final ChatConnectionManager chatConnectionManager;
     private final SessionAttributeAccessor sessionAttributeAccessor;
     private final RabbitTemplate rabbitTemplate;
 
-    public ChatMessageSliceResponse getChatRoomByTradeRequest(Long usedItemId, Long ownerId, Long participantId) { // TODO implement
-        if (Objects.equals(ownerId, participantId)) throw new CustomException(ErrorCode.SELF_CHAT_NOT_ALLOWED);
-        Optional<ChatRoom> chatRoom = chatRoomRepository.findByUsedItemIdAndParticipantId(usedItemId, participantId);
-        if(chatRoom.isEmpty()) {
-            Long chatRoomId = chatRoomRepository.save(ChatRoom.create(usedItemId, ownerId, participantId)).getId();
-            chatMemberRepository.saveAll(List.of(ChatMember.create(chatRoomId, ownerId), ChatMember.create(chatRoomId, participantId)));
-            return chatMessageRepository.getLastMessages(chatRoomId, LocalDateTime.now());
-        } else {
-            return chatMessageRepository.getLastMessages(chatRoom.get().getId(), LocalDateTime.now());
-        }
+    public ChatMessageSliceResponse getChatRoomByTradeRequest(Long usedItemId, Long ownerId, Long participantId) {
+        ChatRoom chatRoom = chatRoomManager.getOrCreate(usedItemId, ownerId, participantId);
+        return chatMessageRepository.getLastMessages(chatRoom.getId(), LocalDateTime.now());
     }
 
     public SliceResponse<ChatRoomListResponse> getChatRooms(Long memberId, LocalDateTime cursor) {
