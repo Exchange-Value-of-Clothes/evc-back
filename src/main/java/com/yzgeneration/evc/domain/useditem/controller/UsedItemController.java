b4fde2003007c4366b124c8f4b2ac792cdd92628
jiyunio <jiyun3269@naver.com>
2025-03-13 13:20:33 +0900

    [Refactor] 생성에 대한 response 값 변경 #119

--- src/main/java/com/yzgeneration/evc/domain/useditem/controller/UsedItemController.java
+++ src/main/java/com/yzgeneration/evc/domain/useditem/controller/UsedItemController.java
@@ -1,7 +1,7 @@
 package com.yzgeneration.evc.domain.useditem.controller;
 
+import com.yzgeneration.evc.common.dto.CommonResponse;
 import com.yzgeneration.evc.domain.useditem.dto.UsedItemRequest.CreateUsedItemRequest;
-import com.yzgeneration.evc.domain.useditem.dto.UsedItemResponse.CreateUsedItemResponse;
 import com.yzgeneration.evc.domain.useditem.dto.UsedItemResponse.LoadUsedItemResponse;
 import com.yzgeneration.evc.domain.useditem.dto.UsedItemResponse.LoadUsedItemsResponse;
 import com.yzgeneration.evc.domain.useditem.service.UsedItemService;
@@ -11,9 +11,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/useditems")
@@ -21,10 +18,10 @@ import java.util.List;
 public class UsedItemController {
     private final UsedItemService usedItemService;
 
-    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public CreateUsedItemResponse createUsedItem(@AuthenticationPrincipal MemberPrincipal memberPrincipal, @RequestPart @Valid CreateUsedItemRequest createUsedItemRequest, @RequestPart List<MultipartFile> imageFiles) {
-        //토큰으로 하여금 회원 정보 받아오기 추가
-        return usedItemService.createUsedItem(memberPrincipal.getId(), createUsedItemRequest, imageFiles);
+    @PostMapping
+    public CommonResponse createUsedItem(@AuthenticationPrincipal MemberPrincipal memberPrincipal, @Valid @RequestBody CreateUsedItemRequest createUsedItemRequest) {
+        usedItemService.createUsedItem(memberPrincipal.getId(), createUsedItemRequest);
+        return CommonResponse.success();
     }
 
     @GetMapping
@@ -36,4 +33,5 @@ public class UsedItemController {
     public LoadUsedItemResponse getUsedItems(@AuthenticationPrincipal MemberPrincipal memberPrincipal, @PathVariable Long usedItemId) {
         return usedItemService.loadUsedItem(memberPrincipal.getId(), usedItemId);
     }
+
 }
