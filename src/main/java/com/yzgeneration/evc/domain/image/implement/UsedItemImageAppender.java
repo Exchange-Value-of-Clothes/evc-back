a2c23e77442e0668effe6f593747783af300da2b
jiyunio <jiyun3269@naver.com>
2025-03-17 10:26:49 +0900

    [Refactor] ImageHandler 클래스명 변경 #119

--- src/main/java/com/yzgeneration/evc/domain/image/implement/UsedItemImageAppender.java
+++ src/main/java/com/yzgeneration/evc/domain/image/implement/UsedItemImageAppender.java
@@ -6,15 +6,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
 public class UsedItemImageAppender {
     private final UsedItemImageRepository usedItemImageRepository;
-    private final S3ImageHandler s3ImageHandler;
+    private final ImageHandler imageHandler;
 
     public void createUsedItemImages(Long itemId, List<String> imageNames) {
-        List<String> imageURLs = imageNames.stream().map(s3ImageHandler::getImageUrl).toList();
-        usedItemImageRepository.saveAll(imageURLs.stream().map(imageURL -> UsedItemImage.create(itemId, imageURL)).toList());
+        List<String> imageURLs = imageNames.stream().map(imageHandler::getImageUrl).toList();
+        //첫 번째 사진 == 썸네일 사진
+        String thumbnailImage = imageURLs.get(0);
+        usedItemImageRepository.saveAll(imageURLs.stream().map(imageURL -> UsedItemImage.create(itemId, imageURL, Objects.equals(thumbnailImage, imageURL))).toList());
     }
 }
