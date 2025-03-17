fa038f46af7c49bf68cc7fa225941fe17951e06a
jiyunio <jiyun3269@naver.com>
2025-03-17 10:26:18 +0900

    [Refactor] ImageHandler 클래스명 변경 #119

--- src/main/java/com/yzgeneration/evc/domain/image/service/ImageService.java
+++ src/main/java/com/yzgeneration/evc/domain/image/service/ImageService.java
@@ -1,7 +1,7 @@
 package com.yzgeneration.evc.domain.image.service;
 
 import com.yzgeneration.evc.domain.image.dto.ImageResponse;
-import com.yzgeneration.evc.domain.image.implement.S3ImageHandler;
+import com.yzgeneration.evc.domain.image.implement.ImageHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,12 +11,12 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class ImageService {
-    private final S3ImageHandler s3ImageHandler;
+    private final ImageHandler imageHandler;
 
     public List<ImageResponse> generatePresignedURL(String prefix, List<MultipartFile> imageFiles) {
         return imageFiles.stream().map(imageFile ->
                 ImageResponse.builder()
-                        .presignedURL(s3ImageHandler.getPresignedURLForUpload(prefix, imageFile.getOriginalFilename()))
+                        .presignedURL(imageHandler.getPresignedURLForUpload(prefix, imageFile.getOriginalFilename()))
                         .imageName(imageFile.getOriginalFilename())
                         .build()
         ).toList();
