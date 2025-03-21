67d6673ddad784c432bab966d705bba180496ad8
jiyunio <jiyun3269@naver.com>
2025-03-22 02:52:13 +0900

    [Refactor] 오타 수정 & nullpoint 방지를 위한 list 초기화 추가 #119

--- src/main/java/com/yzgeneration/evc/domain/useditem/dto/UsedItemRequest.java
+++ src/main/java/com/yzgeneration/evc/domain/useditem/dto/UsedItemRequest.java
@@ -5,11 +5,10 @@ import com.yzgeneration.evc.domain.useditem.enums.TransactionMode;
 import com.yzgeneration.evc.domain.useditem.enums.TransactionType;
 import com.yzgeneration.evc.validator.EnumValidator;
 import com.yzgeneration.evc.validator.Validatable;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UsedItemRequest {
@@ -17,25 +16,19 @@ public class UsedItemRequest {
     @NoArgsConstructor
     public static class CreateUsedItemRequest implements Validatable {
 
-        @NotBlank(message = "제목은 필수항목입니다.")
         private String title;
 
-        @NotBlank(message = "카테고리는 필수항목입니다.")
         private String category;
 
-        @NotBlank(message = "내용은 필수항목입니다.")
         private String content;
 
-        @NotNull(message = "가격은 필수항목입니다.")
         private int price;
 
-        @NotNull(message = "거래유형을 선택해주세요.")
         private String transactionType;
 
-        @NotNull(message = "거래방법을 선택해주세요.")
         private String transactionMode;
 
-        private List<String> imageNames;
+        private List<String> imageNames = new ArrayList<>();
 
         @JsonCreator
         public CreateUsedItemRequest(String title, String category, String content, int price, String transactionType, String transactionMode, List<String> imageNames) {
@@ -51,7 +44,7 @@ public class UsedItemRequest {
 
         @Override
         public void valid() {
-            EnumValidator.validate(TransactionType.class, "trasactionType", transactionType);
+            EnumValidator.validate(TransactionType.class, "transactionType", transactionType);
             EnumValidator.validate(TransactionMode.class, "transactionMode", transactionMode);
         }
     }
