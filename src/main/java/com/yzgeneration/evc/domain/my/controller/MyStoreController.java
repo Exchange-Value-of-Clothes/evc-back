6009ca7736787a222d7af019bbe6a0fcff8c590e
jiyunio <jiyun3269@naver.com>
2025-05-16 01:23:51 +0900

    [Refactor] 내 상점과 관련된 API를 my로 옮김 - 내 중고상품 상태 수정 - 내 중고상품 삭제 - 내 경매상품 삭제
    #208

---
+++ src/main/java/com/yzgeneration/evc/domain/my/controller/MyStoreController.java
@@ -0,0 +1,55 @@
+package com.yzgeneration.evc.domain.my.controller;
+
+import com.yzgeneration.evc.common.dto.CommonResponse;
+import com.yzgeneration.evc.domain.item.auctionitem.dto.MyOrMemberAuctionItemsResponse;
+import com.yzgeneration.evc.domain.item.auctionitem.service.AuctionItemService;
+import com.yzgeneration.evc.domain.item.enums.TransactionMode;
+import com.yzgeneration.evc.domain.item.enums.TransactionStatus;
+import com.yzgeneration.evc.domain.item.useditem.dto.MyOrMemberUsedItemsResponse;
+import com.yzgeneration.evc.domain.item.useditem.service.UsedItemService;
+import com.yzgeneration.evc.domain.my.service.MyStoreService;
+import com.yzgeneration.evc.security.MemberPrincipal;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/api/my/store")
+@RequiredArgsConstructor
+public class MyStoreController {
+    private final MyStoreService myStoreService;
+    private final UsedItemService usedItemService;
+    private final AuctionItemService auctionItemService;
+
+    @GetMapping("/useditems")
+    public MyOrMemberUsedItemsResponse getUsedItems(@AuthenticationPrincipal MemberPrincipal memberPrincipal, @RequestParam(value = "cursor", required = false) LocalDateTime cursor,
+                                                    @RequestParam(value = "condition", required = false) TransactionMode condition) {
+        return myStoreService.getMyUsedItems(memberPrincipal.getId(), cursor, condition);
+    }
+
+    @GetMapping("/auctionitems")
+    public MyOrMemberAuctionItemsResponse getAuctionItmes(@AuthenticationPrincipal MemberPrincipal memberPrincipal, @RequestParam(value = "cursor", required = false) LocalDateTime cursor) {
+        return myStoreService.getMyAuctionItems(memberPrincipal.getId(), cursor);
+    }
+
+    @PatchMapping("/useditems/{usedItemId}")
+    public CommonResponse updateTransactionStatus(@AuthenticationPrincipal MemberPrincipal memberPrincipal, @PathVariable Long usedItemId, @Valid @RequestParam TransactionStatus transactionStatus) {
+        usedItemService.updateTransactionStatus(memberPrincipal.getId(), usedItemId, transactionStatus);
+        return CommonResponse.success();
+    }
+
+    @DeleteMapping("/useditems/{usedItemId}")
+    public CommonResponse deleteUsedItem(@AuthenticationPrincipal MemberPrincipal memberPrincipal, @PathVariable Long usedItemId) {
+        usedItemService.deleteUsedItem(memberPrincipal.getId(), usedItemId);
+        return CommonResponse.success();
+    }
+
+    @DeleteMapping("/auctionitems/{auctionItemId}")
+    public CommonResponse deleteAuctionItem(@AuthenticationPrincipal MemberPrincipal memberPrincipal, @PathVariable Long auctionItemId) {
+        auctionItemService.deleteAuctionItem(memberPrincipal.getId(), auctionItemId);
+        return CommonResponse.success();
+    }
+}
---
+++ src/main/java/com/yzgeneration/evc/domain/my/service/MyStoreService.java
@@ -0,0 +1,26 @@
+package com.yzgeneration.evc.domain.my.service;
+
+import com.yzgeneration.evc.domain.item.auctionitem.dto.MyOrMemberAuctionItemsResponse;
+import com.yzgeneration.evc.domain.item.auctionitem.service.AuctionItemService;
+import com.yzgeneration.evc.domain.item.enums.TransactionMode;
+import com.yzgeneration.evc.domain.item.useditem.dto.MyOrMemberUsedItemsResponse;
+import com.yzgeneration.evc.domain.item.useditem.service.UsedItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class MyStoreService {
+    private final UsedItemService usedItemService;
+    private final AuctionItemService auctionItemService;
+
+    public MyOrMemberUsedItemsResponse getMyUsedItems(Long memberId, LocalDateTime cursor, TransactionMode transactionMode) {
+        return usedItemService.getMyOrMemberUsedItems(memberId, cursor, transactionMode);
+    }
+
+    public MyOrMemberAuctionItemsResponse getMyAuctionItems(Long memberId, LocalDateTime cursor) {
+        return auctionItemService.getMyOrMemberAuctionItems(memberId, cursor);
+    }
+}
