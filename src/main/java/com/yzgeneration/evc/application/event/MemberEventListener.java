3c1f4d963ca4dd03dff113a4520fbe0fbdb9064b
gujunhyeong <rnwnsgud90@naver.com>
2025-05-19 20:04:19 +0900

    [Fix] 이벤트 처리 정상화

--- src/main/java/com/yzgeneration/evc/application/event/MemberEventListener.java
+++ src/main/java/com/yzgeneration/evc/application/event/MemberEventListener.java
@@ -6,9 +6,8 @@ import com.yzgeneration.evc.domain.member.MemberCreatedEvent;
 import com.yzgeneration.evc.domain.point.infrastructure.MemberPointRepository;
 import com.yzgeneration.evc.domain.point.model.MemberPoint;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
@@ -17,15 +16,13 @@ public class MemberEventListener {
     private final MemberPointRepository memberPointRepository;
     private final ProfileImageRepository profileImageRepository;
 
-    @Async
-    @EventListener
+    @TransactionalEventListener
     public void initPoint(MemberCreatedEvent event) {
         Long memberId = event.getMemberId();
         memberPointRepository.save(MemberPoint.create(memberId, 0));
     }
 
-    @Async
-    @EventListener
+    @TransactionalEventListener
     public void initProfileImage(MemberCreatedEvent event) {
         Long memberId = event.getMemberId();
         profileImageRepository.save(ProfileImage.create(memberId, null, event.getImageUrl()));
