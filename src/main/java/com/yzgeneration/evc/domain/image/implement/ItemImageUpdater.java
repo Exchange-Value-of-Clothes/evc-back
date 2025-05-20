1ac8c79b3a58805613aae44fd94654014052dc68
jiyunio <jiyun3269@naver.com>
2025-05-20 16:11:12 +0900

    [Refactor] 상품 내용 수정시 이미지 처리 과정 수정 & s3 내의 이미지 제거 추가 #220

--- src/main/java/com/yzgeneration/evc/domain/image/implement/ItemImageUpdater.java
+++ src/main/java/com/yzgeneration/evc/domain/image/implement/ItemImageUpdater.java
@@ -4,13 +4,11 @@ import com.yzgeneration.evc.domain.image.model.ItemImage;
 import com.yzgeneration.evc.domain.image.service.port.ItemImageRepository;
 import com.yzgeneration.evc.domain.item.enums.ItemType;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ItemImageUpdater {
@@ -21,7 +19,6 @@ public class ItemImageUpdater {
     @Transactional
     public void updateItemImage(Long itemId, ItemType itemType, List<String> addImageNames, List<String> removeImageNames, String thumbnailImage) {
         //썸네일 취소
-        log.info("updateItemImage : itemId - {}, itemType - {}", itemId, itemType);
         ItemImage itemImage = itemImageRepository.findThumbnailByItemIdAndItemType(itemId, itemType);
         itemImage.toggleIsThumbnail();
         itemImageRepository.save(itemImage);
--- src/main/java/com/yzgeneration/evc/domain/my/implement/MyItemUpdater.java
+++ src/main/java/com/yzgeneration/evc/domain/my/implement/MyItemUpdater.java
@@ -9,10 +9,8 @@ import com.yzgeneration.evc.domain.item.useditem.service.port.UsedItemRepository
 import com.yzgeneration.evc.domain.my.dto.MyAuctionItemUpdateRequest;
 import com.yzgeneration.evc.domain.my.dto.MyUsedItemUpdateRequest;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class MyItemUpdater {
@@ -23,30 +21,25 @@ public class MyItemUpdater {
 
     public void updateUsedItem(Long usedItemId, MyUsedItemUpdateRequest myUsedItemUpdateRequest) {
         UsedItem usedItem = usedItemRepository.getById(usedItemId);
-        if (myUsedItemUpdateRequest.getAddImageNames().isEmpty() && myUsedItemUpdateRequest.getRemoveImageNames().isEmpty()) {
-            // 변경할 이미지가 없으므로 처리하지 않음
-            return;
+        if (!myUsedItemUpdateRequest.getAddImageNames().isEmpty() || !myUsedItemUpdateRequest.getRemoveImageNames().isEmpty()) {
+            itemImageUpdater.updateItemImage(usedItem.getId(), ItemType.USEDITEM, myUsedItemUpdateRequest.getAddImageNames(), myUsedItemUpdateRequest.getRemoveImageNames(), myUsedItemUpdateRequest.getThumbnailImage());
         }
-        log.info("updateUsedItem : {}", usedItem.getItemDetails().getTitle());
-        itemImageUpdater.updateItemImage(usedItem.getId(), ItemType.USEDITEM, myUsedItemUpdateRequest.getAddImageNames(), myUsedItemUpdateRequest.getRemoveImageNames(), myUsedItemUpdateRequest.getThumbnailImage());
+
         usedItem.update(myUsedItemUpdateRequest);
         usedItemRepository.save(usedItem);
     }
 
     public void updateAuctionItem(Long auctionItemId, MyAuctionItemUpdateRequest myAuctionItemUpdateRequest) {
         AuctionItem auctionItem = auctionItemRepository.getById(auctionItemId);
-        if (myAuctionItemUpdateRequest.getAddImageNames().isEmpty() && myAuctionItemUpdateRequest.getRemoveImageNames().isEmpty()) {
-            return;
+        if (!myAuctionItemUpdateRequest.getAddImageNames().isEmpty() || !myAuctionItemUpdateRequest.getRemoveImageNames().isEmpty()) {
+            itemImageUpdater.updateItemImage(auctionItem.getId(), ItemType.AUCTIONITEM, myAuctionItemUpdateRequest.getAddImageNames(), myAuctionItemUpdateRequest.getRemoveImageNames(), myAuctionItemUpdateRequest.getThumbnailImage());
         }
 
         if (auctionItemRepository.countParticipantById(auctionItem.getId()) > 0) { //참여자가 있으면 시가 변경 X
-            itemImageUpdater.updateItemImage(auctionItem.getId(), ItemType.AUCTIONITEM, myAuctionItemUpdateRequest.getAddImageNames(), myAuctionItemUpdateRequest.getRemoveImageNames(), myAuctionItemUpdateRequest.getThumbnailImage());
             auctionItem.updateIfParticipantExists(myAuctionItemUpdateRequest);
-            auctionItemRepository.save(auctionItem);
         } else {
-            itemImageUpdater.updateItemImage(auctionItem.getId(), ItemType.AUCTIONITEM, myAuctionItemUpdateRequest.getAddImageNames(), myAuctionItemUpdateRequest.getRemoveImageNames(), myAuctionItemUpdateRequest.getThumbnailImage());
             auctionItem.update(myAuctionItemUpdateRequest);
-            auctionItemRepository.save(auctionItem);
         }
+        auctionItemRepository.save(auctionItem);
     }
 }
