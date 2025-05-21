44b423e8cc90fbc3a7ec6105630cdd148c5a29da
jiyunio <jiyun3269@naver.com>
2025-05-21 21:46:59 +0900

    [Refactor] rabbitmq listener 재큐 안되도록 설정

--- src/main/java/com/yzgeneration/evc/domain/item/auctionitem/implement/AuctionBidListener.java
+++ src/main/java/com/yzgeneration/evc/domain/item/auctionitem/implement/AuctionBidListener.java
@@ -5,6 +5,7 @@ import com.yzgeneration.evc.domain.item.auctionitem.dto.AuctionBidToListener;
 import com.yzgeneration.evc.domain.item.auctionitem.service.port.AuctionItemRepository;
 import com.yzgeneration.evc.domain.item.auctionitem.service.port.AuctionRoomRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
@@ -18,8 +19,12 @@ public class AuctionBidListener {
 
     @RabbitListener(queues = "auction.queue")
     public void receiveAuctionBidMessage(AuctionBidToListener auctionBidToListener) {
-        Long auctionItemId = auctionRoomRepository.findAuctionItemIdById(auctionBidToListener.getAuctionRoomId());
-        messagingTemplate.convertAndSend("/topic/auction-room." + auctionBidToListener.getAuctionRoomId(),
-                new AuctionBidResponse(auctionBidToListener.getBidderId(), auctionBidToListener.getCurrentPrice(), auctionItemRepository.countParticipantById(auctionItemId)));
+        try {
+            Long auctionItemId = auctionRoomRepository.findAuctionItemIdById(auctionBidToListener.getAuctionRoomId());
+            messagingTemplate.convertAndSend("/topic/auction-room." + auctionBidToListener.getAuctionRoomId(),
+                    new AuctionBidResponse(auctionBidToListener.getBidderId(), auctionBidToListener.getCurrentPrice(), auctionItemRepository.countParticipantById(auctionItemId)));
+        } catch (Exception e) {
+            throw new AmqpRejectAndDontRequeueException("경매 처리 실패: 재큐 금지", e);
+        }
     }
 }
